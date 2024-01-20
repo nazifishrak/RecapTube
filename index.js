c248e0348e2b41fc1f6b65193aeb9b5023528d9c
nazifishrak <nzfishrak60@gmail.com>
2024-01-20 12:35:57 -0800

    p1

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ import { YoutubeTranscript } from 'youtube-transcript';
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 
+
+app.get("/", (req, res) =>{
+    res.send("<h1>ITS WORKING</h1>")
+})
+
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
+
+
 
 
 
