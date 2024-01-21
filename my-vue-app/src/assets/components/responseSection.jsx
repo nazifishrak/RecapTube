4ec2ee54da99508de63931d7c57eede7b8e32f8d
maharajhaider <41383858+maharajhaider@users.noreply.github.com>
2024-01-20 19:02:52 -0800

    - herosection updated -backend response connected

--- my-vue-app/src/assets/components/responseSection.jsx
+++ my-vue-app/src/assets/components/responseSection.jsx
@@ -1,15 +1,17 @@
+import './ResponseSection.css'
 const ResponseSection = ({keypoints, notes}) => {
 
     return(
-        <div className="flex justify-evenly">
-            <div id="card1" className="ml-20 mx-4 my-8 w-72 h-125 bg-white border border-gray-300 rounded-lg shadow-md p-8 clickable-card transition duration-300">
+        <div className="cards-container">
+
+            <div id="card1">
                 <p className="text-xl font-bold mb-4 text-gray-800">Wide Card</p>
                 <p className="text-gray-700">
                     {keypoints}
                 </p>
             </div>
 
-            <div id="card2" className="mr-20 mx-4 my-8 w-100 h-72 bg-white border border-gray-300 rounded-lg shadow-md p-8 clickable-card transition duration-300">
+            <div id="card2">
                 <p className="text-xl font-bold mb-4 text-gray-800">Tall Card</p>
                 <p className="text-gray-700">
                     {notes}
